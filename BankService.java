67389a6bc780ca8f82a9b0ef696377c36793b406
ShraddaMshetty <shraddhamshetty2308@gmail.com>
2026-04-24 10:03:36 +0530

    Update BankService.java

--- BankService.java
+++ BankService.java
@@ -1,7 +1,7 @@
 class BankService {
     public static void main(String[] args) {
         int balance = 1000;
-        int deposit = 500;
+        int deposit = 5000;
         int withdraw = 300;
 
         balance = balance + deposit;
